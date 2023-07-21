fdabe4946da7de9b9f9fba871bca590700b98a5e
Olha-buz <134137848+Olha-buz@users.noreply.github.com>
2023-07-21 22:41:56 +0300

    gmdcgf

--- reviews.js
+++ reviews.js
@@ -1,12 +1,28 @@
+var slideIndex = 1;
+showSlides(slideIndex);
 
-(function($) {
-$(function() {
-  
-  $('.formRadio.radio').on('click', '.radio:not(.active)', function() {
-    $(this)
-      .addClass('active').siblings().removeClass('active')
-      .closest('div-reviewes').find('reviewes').removeClass('active').eq($(this).index()).addClass('active');
-  });
-  
-});
-})(jQuery);
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
